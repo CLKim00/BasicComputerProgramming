18ed1c9a57410bdc49ac658f8bd6de83409a419f
DSKim <108538247+CLKim00@users.noreply.github.com>
2023-06-04 02:13:40 +0900

    scanf()의 사용

---
+++ REP3_3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    double a, b, c, dis;
+
+    printf("계수를 입력하시오: ");  scanf("%lf", &a);
+    printf("계수를 입력하시오: ");  scanf("%lf", &b);
+    printf("계수를 입력하시오: ");  scanf("%lf", &c);
+
+}
